d4de85d53837ab3fd8d88c69122856473c3ef3ad
Yujin Kim <yujin71@gmail.com>
2024-09-09 11:00:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -264,8 +264,8 @@ def submit():
 
     # Commit the changes to the database
     mysql.commit()
-    cursor.close()
-    mysql.close() 
+    # cursor.close()
+    # mysql.close() 
 
     return render_template('result.html', Sensitivity_level=Sensitivity_level, Assertiveness_level=Assertiveness_level, Reflector_level=Reflector_level, results=results)
 
